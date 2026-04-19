888c5279b29e119864d589cbadc5123299e5e859
CubingXT <mrkadunsta@gmail.com>
2026-04-19 18:30:22 -0500

    Fix Platformer

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ bool isTrackable(PlayLayer* pl) {
     if (!pl || !pl->m_level) return false;
     if (pl->m_isPracticeMode) return false;
     if (pl->m_isTestMode) return false;
-    if (pl->m_level->m_isPlatformer) return false;
+    if (pl->m_isPlatformer) return false;
     return true;
 }
 
