80730ffe74f59cfa0e22bc57b22f3d5df72986ba
CubingXT <mrkadunsta@gmail.com>
2026-04-19 20:31:39 -0500

    Reformat bc it looks nicer

--- src/utils.hpp
+++ src/utils.hpp
@@ -5,11 +5,11 @@
 
 using namespace geode::prelude;
 
-inline std::string getLevelKey(GJGameLevel* level) {
-    if (level->m_levelType == GJLevelType::Editor) {
-        return fmt::format("editor-{}", EditorIDs::getID(level));
-    } else if (level->m_levelType == GJLevelType::Main) {
-        return fmt::format("main-{}", EditorIDs::getID(level));
-    }
-    return fmt::format("level-{}", EditorIDs::getID(level));
+inline std::string getLevelKey(GJGameLevel *level) {
+  if (level->m_levelType == GJLevelType::Editor) {
+    return fmt::format("editor-{}", EditorIDs::getID(level));
+  } else if (level->m_levelType == GJLevelType::Main) {
+    return fmt::format("main-{}", EditorIDs::getID(level));
+  }
+  return fmt::format("level-{}", EditorIDs::getID(level));
 }
